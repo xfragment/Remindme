e029ee91411f069888c7fde217efb2000c460648
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-03-31 14:42:16 +0530

    backend code added

---
+++ backend/models/product.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxLength: 32,
+    },
+    description: {
+      type: String,
+      required: true,
+      maxLength: 2000,
+    },
+    price: {
+      type: Number,
+      trim: true,
+      required: true,
+      maxLength: 32,
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+      required: true,
+      maxLength: 32,
+    },
+    quantity: {
+      type: Number,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+    shipping: {
+      required: false,
+      type: Boolean,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Product", productSchema);
